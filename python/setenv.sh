82a3069d134f77e2b29092fd32855564a705434a
Jonathan Grot <jonathan.grot@gmail.com>
2021-01-24 11:23:54 -0500

    Updated setenv for MacOS using brew's greadlink

--- python/setenv.sh
+++ python/setenv.sh
@@ -1,7 +1,13 @@
 # Sets up these python libraries in user's python path
 
 # BASH_SOURCE holds the name of the script being sourced
-SCRIPTNAME=$(readlink -f $BASH_SOURCE)
+if [ $(uname) == "Linux" ]; then
+    SCRIPTNAME=$(readlink -f $BASH_SOURCE)
+elif [ $(uname) == "Darwin" ]; then
+    SCRIPTNAME=$(greadlink -f $BASH_SOURCE)
+else
+    echo "***ERROR: unrecognized host OS"
+fi
 
 # % removes the shortest matching pattern
 # /* expands to "/<and any other characters>"
@@ -13,9 +19,12 @@ KSPCALCDIR=$LIBDIR/kspcalc
 NEWPYTHONPATH=$LIBDIR:$KSPCALCDIR
 
 if ! echo $PYTHONPATH | grep -q "$NEWPYTHONPATH"; then
+    
     if [ -z $PYTHONPATH ]; then
 	export PYTHONPATH=$NEWPYTHONPATH
     else
 	export PYTHONPATH=$PYTHONPATH:$NEWPYTHONPATH
     fi
+
+    echo "PYTHONPATH HAS BEEN UPDATED TO $PYTHONPATH"
 fi
